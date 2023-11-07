45d937335900b8dab3d458fcd54fcdd5b503f56e
KIBI312 <isken00.kg@gmail.com>
2023-11-07 14:32:05 +0600

    Initial commit

---
+++ src/main/java/com/seitov/gymworkload/GymWorkloadApplication.java
@@ -0,0 +1,13 @@
+package com.seitov.gymworkload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymWorkloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymWorkloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seitov/gymworkload/GymWorkloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seitov.gymworkload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymWorkloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
