8d3b67abbab1870a96546650cdedb0945b90fced
KIBI312 <isken00.kg@gmail.com>
2023-11-29 13:29:58 +0600

    Adds messaging support for reporting service

---
+++ src/main/java/com/seitov/gymworkload/config/JmsConfig.java
@@ -0,0 +1,38 @@
+package com.seitov.gymworkload.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.seitov.gymworkload.dto.ReportRecordDto;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        Map<String, Class<?>> typeIdMappings = new HashMap<String, Class<?>>();
+        typeIdMappings.put("TrainingReportDto", ReportRecordDto.class);
+        converter.setObjectMapper(objectMapper());
+        converter.setTypeIdMappings(typeIdMappings);
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return JsonMapper.builder()
+                .addModule(new JavaTimeModule())
+                .build();
+    }
+
+}
--- src/main/java/com/seitov/gymworkload/service/impl/ReportRecordServiceImpl.java
+++ src/main/java/com/seitov/gymworkload/service/impl/ReportRecordServiceImpl.java
@@ -7,6 +7,7 @@ import com.seitov.gymworkload.service.ReportRecordService;
 import com.seitov.gymworkload.service.ReportService;
 import ma.glasnost.orika.MapperFacade;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +23,7 @@ public class ReportRecordServiceImpl implements ReportRecordService {
 
     @Override
     @Transactional
+    @JmsListener(destination = "${spring.activemq.queue}")
     public void createRecord(ReportRecordDto dto) {
         ReportRecord reportRecord = orikaMapper.map(dto, ReportRecord.class);
         repository.save(reportRecord);
